eb5e7380ca5d3666f1169dd670eab021ce797fbb
elielcena <45287391+elielcena@users.noreply.github.com>
2021-03-20 11:11:59 -0300

    Project cenafood-api created with configured database

---
+++ cenafood-api/src/main/java/com/github/cenafood/CenafoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.cenafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenafoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenafoodApiApplication.class, args);
+	}
+
+}
---
+++ cenafood-api/src/test/java/com/github/cenafood/CenafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cenafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
