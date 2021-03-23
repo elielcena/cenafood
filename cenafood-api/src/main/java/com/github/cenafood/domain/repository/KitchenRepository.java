4207124f97f88085d1e0ddf24c9e01f2fb225ea3
elielcena <45287391+elielcena@users.noreply.github.com>
2021-03-23 11:14:22 -0300

    Update kitchen and Restaurant Repository

--- cenafood-api/src/main/java/com/github/cenafood/domain/repository/KitchenRepository.java
+++ cenafood-api/src/main/java/com/github/cenafood/domain/repository/KitchenRepository.java
@@ -1,5 +1,8 @@
 package com.github.cenafood.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +14,11 @@ import com.github.cenafood.domain.model.Kitchen;
  */
 @Repository
 public interface KitchenRepository extends JpaRepository<Kitchen, Long> {
+
+	List<Kitchen> findAllByNameContaining(String name);
+
+	Optional<Kitchen> findByName(String name);
+
+	boolean existsByName(String name);
 
 }
--- cenafood-api/src/main/java/com/github/cenafood/domain/repository/RestaurantRepository.java
+++ cenafood-api/src/main/java/com/github/cenafood/domain/repository/RestaurantRepository.java
@@ -1,6 +1,9 @@
 package com.github.cenafood.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.github.cenafood.domain.model.Restaurant;
@@ -11,5 +14,13 @@ import com.github.cenafood.domain.model.Restaurant;
  */
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
+
+	static final String JPQL_RESTAURANT = "FROM Restaurant r JOIN FETCH r.kitchen k";
+
+	@Query(JPQL_RESTAURANT)
+	List<Restaurant> findAll();
+
+	@Query(JPQL_RESTAURANT + " WHERE name LIKE %:name% AND k.id = :id")
+	List<Restaurant> findByName(String name, Long id);
 
 }
