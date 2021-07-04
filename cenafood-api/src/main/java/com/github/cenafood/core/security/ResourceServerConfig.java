47f4ccf783eea1215b404cea4d5de1d73e52a6bb
Eliel Cena <elielsenna1999@gmail.com>
2021-07-04 11:41:30 -0300

    Setting resource server for authorization with cenafood-auth

---
+++ cenafood-api/src/main/java/com/github/cenafood/core/security/ResourceServerConfig.java
@@ -0,0 +1,25 @@
+package com.github.cenafood.core.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author elielcena
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer().opaqueToken();
+    }
+
+}
