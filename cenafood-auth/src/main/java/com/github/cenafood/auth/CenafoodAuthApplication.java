07534f188fdf9bf80e8cf4c70df7f25b8afd375a
Eliel Cena <elielsenna1999@gmail.com>
2021-07-04 10:21:46 -0300

    Cenafood authorization project created

---
+++ cenafood-auth/src/main/java/com/github/cenafood/auth/CenafoodAuthApplication.java
@@ -0,0 +1,13 @@
+package com.github.cenafood.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenafoodAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CenafoodAuthApplication.class, args);
+	}
+
+}
---
+++ cenafood-auth/src/test/java/com/github/cenafood/auth/CenafoodAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.cenafood.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CenafoodAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
