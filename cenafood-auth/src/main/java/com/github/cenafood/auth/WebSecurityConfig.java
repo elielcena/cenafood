77e77bdcfe4cf7818a6117f39239c552b8a7d7e3
Eliel Cena <elielsenna1999@gmail.com>
2021-07-04 11:29:08 -0300

    authorization grand type password & refresh token implemented

---
+++ cenafood-auth/src/main/java/com/github/cenafood/auth/WebSecurityConfig.java
@@ -0,0 +1,37 @@
+package com.github.cenafood.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author elielcena
+ *
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("cena").password(passwordEncoder().encode("cena")).roles("ADMIN")
+                .and()
+                .withUser("eliel").password(passwordEncoder().encode("eliel")).roles("CUSTOMER");
+    }
+
+}
